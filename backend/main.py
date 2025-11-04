cf1346a58bfb1e2ee34eb3e11f5a1b00be1bf073
mboyakanni <raheemmubarak7@gmail.com>
2025-11-04 16:26:52 +0100

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -80,7 +80,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "https://*.vercel.app",  # Add this for Vercel
-        "*"  # Temporary - we'll restrict after deployment
+        "https://truthfi.vercel.app/"  # Temporary - we'll restrict after deployment
     ],
     allow_credentials=True,
     allow_methods=["*"],
