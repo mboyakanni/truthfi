d2e631cfc55b089a6d00503c1b24b76df7a38c45
mboyakanni <raheemmubarak7@gmail.com>
2025-11-04 12:15:21 +0100

    u

--- backend/main.py
+++ backend/main.py
@@ -525,7 +525,7 @@ async def internal_error_handler(request, exc):
 
 if __name__ == "__main__":
     import os
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
